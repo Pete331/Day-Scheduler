66c4b3523ca95afc4c95f77acb42fa7b7ba3f399
Peter Brennan <pweda64@hotmail.com>
2020-03-14 15:14:08 +0800

    updated stylesheet

--- script.js
+++ script.js
@@ -1,57 +1,80 @@
 $(document).ready(function() {
-  // creates a variable of the current date
-  //   var now = moment().format("dddd, MMMM Do YYYY");
+  for (i = 9; i < 19; i++) {
+    $("#row-0")
+      .clone()
+      .attr("id", i - 1)
+      .insertBefore("#row-0");
+    $("#time").attr("id", i - 1);
+    $("#time").text(i + "00");
+    $("#input").attr("id", i - 1);
+    $("#description").attr("id", i - 1);
+  }
 
-  function colourCode (){
-  // for testing
-  var now = moment();
-  now.add(3, 'hours');
-//   now.subtract(7, "hours");
-  var now = now.hour();
+  // once all html elements are created, remove the initial copy as id's dont match up
+  $("#row-0").remove();
 
-  //   displays current date at top of screen
-  $("#currentDay").text(now);
-  console.log(now);
+  function colourCode() {
+    // creates a variable of the current date
+    var nowScreen = moment().format("dddd, MMMM Do YYYY");
+    //   displays current date at top of screen
+    $("#currentDay").text(nowScreen);
 
-  $(".description").each(function() {
-    var timeQuery = parseInt($(this).attr("id"));
-    if (now === timeQuery) {
-      $(this).addClass("present");
-    } else if (now > timeQuery) {
-      $(this).addClass("past");
-    } else {
-      $(this).addClass("future");
-    }
+    var now = moment();
+    // for testing - uncomment one of the below lines and change hour number
+    // now.add(3, 'hours');
+    // now.subtract(7, "hours");
+
+    // gets the moment and returns the hour and stores in now variable
+
+    var now = now.hour();
+    $(".description").each(function() {
+      var timeQuery = parseInt($(this).attr("id"));
+      if (now === timeQuery) {
+        $(this).addClass("present");
+      } else if (now > timeQuery) {
+        $(this).addClass("past");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  function getEntries() {
+    $('[id="8"]').val(JSON.parse(localStorage.getItem("800")));
+    $('[id="9"]').val(JSON.parse(localStorage.getItem("900")));
+    $('[id="10"]').val(JSON.parse(localStorage.getItem("1000")));
+    $('[id="11"]').val(JSON.parse(localStorage.getItem("1100")));
+    $('[id="12"]').val(JSON.parse(localStorage.getItem("1200")));
+    $('[id="13"]').val(JSON.parse(localStorage.getItem("1300")));
+    $('[id="14"]').val(JSON.parse(localStorage.getItem("1400")));
+    $('[id="15"]').val(JSON.parse(localStorage.getItem("1500")));
+    $('[id="16"]').val(JSON.parse(localStorage.getItem("1600")));
+    $('[id="17"]').val(JSON.parse(localStorage.getItem("1700")));
+  }
+
+  $(".saveBtn").click(function() {
+    localStorage.setItem(
+      $(this)
+        .parent()
+        .siblings(".time-block")
+        .text(),
+      JSON.stringify(
+        $(this)
+          .parent()
+          .siblings(".description")
+          .children()
+          .val()
+      )
+    );
   });
-};
-
-function getEntries() {
-  $("#8am-input").val(JSON.parse(localStorage.getItem("8am-storage")));
-}
 
 
-$(".saveBtn").click(function() {
-  localStorage.setItem("8am-storage", JSON.stringify($("#8am-input").val()));
+$(".clear-btn").on("click", function() {
+  window.localStorage.clear();
+  window.location.reload();
 });
 
-// run functions
-getEntries();
-colourCode();
+  // run functions
+  getEntries();
+  colourCode();
 });
-
-
-// not mine
-// $(".saveBtn").click(function() {
-//     words = $(this).siblings(".input").val();
-//     console.log(words);
-//     hourInfo = $(this).siblings(".hour").text();
-//     console.log(hourInfo);
-//     localStorage.setItem(hourInfo, JSON.stringify(words));
-    
-//     colorChange ();
-//     renderText ();
-// })
-
-// var saveWords9 = JSON.parse(localStorage.getItem("9:00 am"));
-// $("#9").val("");
-// $("#9").val(saveWords9);
